6aa62251248d9dc89b13487b6d5d180d9027041e
Jiahao Xu <jiahao.xu@iluvatar.com>
2026-06-22 15:59:58 +0800

    Add FLYDSL_ENABLE_PYTHON option

--- include/flydsl/Conversion/Passes.h
+++ include/flydsl/Conversion/Passes.h
@@ -4,12 +4,22 @@
 #ifndef FLYDSL_CONVERSION_PASSES_H
 #define FLYDSL_CONVERSION_PASSES_H
 
+#include "mlir/Pass/Pass.h"
+
+#if __has_include("flydsl/Conversion/FlyToIXDL/Passes.h.inc")
+#include "flydsl/Conversion/FlyToIXDL/FlyToIXDL.h"
+#endif
+
+#if __has_include("flydsl/Conversion/FlyToROCDL/Passes.h.inc")
 #include "flydsl/Conversion/FlyToROCDL/FlyToROCDL.h"
+#endif
 
 namespace mlir {
 
+#if __has_include("flydsl/Conversion/FlyToROCDL/Passes.h.inc")
 #define GEN_PASS_REGISTRATION
 #include "flydsl/Conversion/FlyToROCDL/Passes.h.inc"
+#endif
 
 } // namespace mlir
 
